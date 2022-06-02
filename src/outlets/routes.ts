fa6976f93e0bd2ef7d54a93b48f2a0efe916bdb3
prahalad <prahlad@webtrigon.com>
2022-06-02 19:53:39 +0530

    Added outlet Api's

--- src/outlets/routes.ts
+++ src/outlets/routes.ts
@@ -1,9 +1,11 @@
-import express from 'express';
+import express, { response } from 'express';
 import { DB_NAME, getDatabaseClient } from '../db-utls';
 import bcrypt from 'bcrypt';
 import asyncHandler from 'express-async-handler';
 import { generateJWT } from '../auth/auth';
 import passport from 'passport';
+import { request } from 'https';
+import { ObjectId } from 'mongodb';
 
 const outletRoutes = express.Router();
 const SALT_ROUNDS = 12;
@@ -55,12 +57,81 @@ outletRoutes.post('/add/', jwtAuthentication, async (request, response) => {
     return;
 });
 
-outletRoutes.get('/', jwtAuthentication,async (request, response) => {
+outletRoutes.get('/', jwtAuthentication, async (request, response) => {
     const outletCollections = getDatabaseClient().db(DB_NAME).collection('outlets');
     const allOutlets = await outletCollections.find().toArray();
 
     response.status(200);
     response.json(allOutlets);
 })
+
+outletRoutes.post('/addUserOutlets/', jwtAuthentication, async (request, response) => {
+    const userID = request.body.userID;
+    const outletID = request.body.outletID;
+    const isAnonymous = request.body.isAnonymous;
+
+    if (!userID || !outletID) {
+        response.status(400);
+        response.send("Incomplete field type")
+    }
+
+    const userCollection = getDatabaseClient().db(DB_NAME).collection('users')
+    const outletCollections = getDatabaseClient().db(DB_NAME).collection('outlets');
+    const selectedOutletsCollections = getDatabaseClient().db(DB_NAME).collection('selectedOutlets');
+
+    const currentUser = await userCollection.findOne({
+        _id: new ObjectId(userID.toString()),
+    });
+
+    const currentOutlet = await outletCollections.findOne({
+        _id: new ObjectId(outletID.toString()),
+    });
+
+    try {
+        const submittedUser = await selectedOutletsCollections.find({
+            userID,
+        }).count();
+
+        if (submittedUser > 0) {
+            response.status(400);
+            response.send('User already submitted the response');
+            return;
+        }
+    } catch {
+        return;
+    }
+
+    try {
+        selectedOutletsCollections.insertOne({
+            userName: isAnonymous ? 'Anonymous' : currentUser.name,
+            userID,
+            outletInfo: currentOutlet
+        });
+        response.send("User submitted response sucessfully");
+    } catch (e) {
+        response.sendStatus(500);
+        response.json(e)
+        response.send("Error Submitting Data");
+        return;
+    }
+
+    return;
+})
+
+outletRoutes.post('/clearAllUserOutlets/', jwtAuthentication, async (request, response) => {
+    const selectedOutletsCollections = getDatabaseClient().db(DB_NAME).collection('selectedOutlets');
+
+    try {
+        selectedOutletsCollections.deleteMany({});
+        response.status(200);
+        response.send("All Data Cleared Succussfully")
+    } catch {
+        return;
+    }
+
+
+    return;
+})
+
 
 export default outletRoutes;
